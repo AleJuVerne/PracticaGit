93741e158a13d3211a551d04914d294a0ce642fe
AleJuVerne <122205216+AleJuVerne@users.noreply.github.com>
2024-02-06 10:51:54 +0100

    Agregado un nuevo syso a prueba.java

--- src/ejemplo/prueba.java
+++ src/ejemplo/prueba.java
@@ -5,6 +5,7 @@ public class prueba {
 	public static void main(String[] args) {
 		System.out.println("Esto es una prueba para git");
 		System.out.println("Nuevo cambio");
+		System.out.println("Este cambio se ha hecho desde GitHub");
 	}
 
 }
