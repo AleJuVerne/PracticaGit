4a349f321bdc8f7364dd85185ff2485a946c4ec6
alejandro <alejandro.vilches.fernandez.alu@iesjulioverne.es>
2024-02-11 18:42:46 +0100

    Nuevo commit desde Spring Boot

--- src/ejemplo/prueba.java
+++ src/ejemplo/prueba.java
@@ -9,7 +9,7 @@ public class prueba {
 		System.out.println("Nuevo cambio desde local");
 		System.out.println("Cambios realizado a esta linea desde eclipse");
 		System.out.println("Linea Agregada desde Spring Boot");
-		System.out.println("Linea Agregada desde GitHub para Spring Boot");
+		System.out.println("Linea de GitHUb modificada en Spring");
 	}
 
 }
